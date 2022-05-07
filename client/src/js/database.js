f5aa4b2747acd4184dec7fcdb6732511981a5ead
PatrickFHam <93452044+PatrickFHam@users.noreply.github.com>
2022-05-07 17:24:12 -0400

    passed "e" into the getDb function, since it's being looked-for elsewhere

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -25,13 +25,13 @@ export const putDb = async (content) => {
 };
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => {
+export const getDb = async (e) => {
   // console.error('getDb not implemented')  THIS IS THE INITIAL ERROR BEFORE I BUILT THIS FUNCTION
   console.log('GET from the database');
   const jateDb = await openDB('jate', 1);
   const tx = jateDb.transaction('jate', 'readonly');
   const store = tx.objectStore('jate');
-  const request = store.getAll();
+  const request = store.get(1);
   const result = await request;
   console.log('result.value', result);
   return result;
