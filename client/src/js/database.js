18300f9a9b053ff1328ec310e9e0513efc988ed7
PatrickFHam <93452044+PatrickFHam@users.noreply.github.com>
2022-05-07 16:41:43 -0400

    first commit in this location, already did a lot of work in BootCamp's "develop" folder ... many bugs to fix

---
+++ client/src/js/database.js
@@ -0,0 +1,40 @@
+import { openDB } from 'idb';
+
+const initdb = async () =>
+  openDB('jate', 1, {
+    upgrade(db) {
+      if (db.objectStoreNames.contains('jate')) {
+        console.log('jate database already exists');
+        return;
+      }
+      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
+      console.log('jate database created');
+    },
+  });
+
+// TODO: Add logic to a method that accepts some content and adds it to the database
+export const putDb = async (content) => {
+  // console.error('putDb not implemented')  THIS IS THE INITIAL ERROR BEFORE I BUILT THIS FUNCTION
+  console.log('Post/PUT to the database');
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  const request = store.add(content);
+  const result = await request;
+  console.log('🚀 - data saved to the jate database', result);
+};
+
+// TODO: Add logic for a method that gets all the content from the database
+export const getDb = async () => {
+  // console.error('getDb not implemented')  THIS IS THE INITIAL ERROR BEFORE I BUILT THIS FUNCTION
+  console.log('GET from the database');
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
+  const request = store.getAll();
+  const result = await request;
+  console.log('result.value', result);
+  return result;
+};
+
+initdb();
