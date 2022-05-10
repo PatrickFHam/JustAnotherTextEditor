f0a42210a623a9ff63ce80378f7a9d53eed38fb5
PatrickFHam <93452044+PatrickFHam@users.noreply.github.com>
2022-05-10 17:50:52 -0400

    comments to webpack and expressjs

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,11 +1,10 @@
+// Imports Webpack, Manifest
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
-
+// Webpack and Manifest options/definitions
 module.exports = () => {
   return {
     mode: 'development',
